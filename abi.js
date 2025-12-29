031b03647250d25c8be752f39cf9941d8c9aa8ca
sumana960 <sumanadiara4@gmail.com>
2025-12-29 17:45:41 +0100

    Add TOKEN_ABI for ERC20 token functions

---
+++ abi.js
@@ -0,0 +1,5 @@
+const TOKEN_ABI = [
+  "function balanceOf(address) view returns (uint256)",
+  "function decimals() view returns (uint8)",
+  "function transfer(address,uint256) returns (bool)"
+];
