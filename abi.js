e277b88c12b6f23aec47f77f81f0a727f2b8433e
sumana960 <sumanadiara4@gmail.com>
2025-12-29 20:22:30 +0100

    Delete abi.js

--- abi.js
+++
@@ -1,5 +0,0 @@
-const TOKEN_ABI = [
-  "function balanceOf(address) view returns (uint256)",
-  "function decimals() view returns (uint8)",
-  "function transfer(address,uint256) returns (bool)"
-];
